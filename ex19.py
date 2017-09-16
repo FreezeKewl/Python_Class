d339003b6a526ebe87849414e2c213cadf9c232b
FreezeKewl <FreezeKewl@users.noreply.github.com>
2017-09-16 11:40:18 -0400

    Add files via upload
    In class assignment

---
+++ ex19.py
@@ -0,0 +1,27 @@
+#define function and arguments cheese and crackers
+
+#print give the numbers directly
+#call function with numbers directly
+
+#print using variables
+#call using variables used in function
+
+# call doing the math inside the call()
+
+# call with combined variable and math
+
+def Cheese_and_Crackers(cheeses, crackers):
+    print(f"How many cheeses are there? \n {cheeses}")
+    print(f"How many crackers are there? \n {crackers}")
+    print("Great, now all you need is something to drink.")
+    print("To bad, nothing to drink!")
+
+Cheese_and_Crackers(32.5,57.1)
+
+cheeses = 12
+crackers = 110
+Cheese_and_Crackers(cheeses, crackers)
+
+Cheese_and_Crackers(10 + 32, 5 + 30)
+
+Cheese_and_Crackers(cheeses + 10, crackers + 15)
